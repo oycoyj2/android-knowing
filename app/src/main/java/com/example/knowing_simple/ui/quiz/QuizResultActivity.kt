9cfd7d9270c43b0420e04a48bafb6f5329d7c791
oycoyj2 <oycoyj2@gmail.com>
2024-09-09 23:45:01 +0900

    퀴즈 결과 페이지 UI 구현
    디자인 작업 때 빼먹어서 간단하게만 구현했다.

--- app/src/main/java/com/example/knowing_simple/ui/quiz/QuizResultActivity.kt
+++ app/src/main/java/com/example/knowing_simple/ui/quiz/QuizResultActivity.kt
@@ -1,6 +1,7 @@
 package com.example.knowing_simple.ui.quiz
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.knowing_simple.R
@@ -8,16 +9,24 @@ import com.example.knowing_simple.R
 class QuizResultActivity : AppCompatActivity() {
 
     private lateinit var resultTextView: TextView
+    private lateinit var confirmButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_result)
 
         resultTextView = findViewById(R.id.resultTextView)
+        confirmButton = findViewById(R.id.confirmButton)
 
         val knownCount = intent.getIntExtra("knownCount", 0)
         val totalCount = intent.getIntExtra("totalCount", 0)
 
-        resultTextView.text = "너는 $knownCount 개 만큼 맞았어. $totalCount 개 중에 말이야!"
+        resultTextView.text = "문제 개수: $totalCount\n맞춘 문제: $knownCount"
+
+        confirmButton.setOnClickListener {
+            finish() // 현재 액티비티 종료하고 이전 화면으로 돌아가기
+        }
     }
+
+
 }
